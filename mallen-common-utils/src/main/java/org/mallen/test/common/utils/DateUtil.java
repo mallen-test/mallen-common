514de0613f36ab881fbea148f29a7901eea3c5da
mallen huang <mallen.huang@timanetworks.com>
2020-01-04 16:50:35 +0800

    add function in DateUtil

--- mallen-common-utils/src/main/java/org/mallen/test/common/utils/DateUtil.java
+++ mallen-common-utils/src/main/java/org/mallen/test/common/utils/DateUtil.java
@@ -1,7 +1,10 @@
 package org.mallen.test.common.utils;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
 
 /**
  * @author mallen
@@ -17,4 +20,110 @@ public class DateUtil {
     public static Long toMilliseconds(LocalDate date) {
         return date.atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli();
     }
+
+    /**
+     * 将时间戳转换为local date
+     *
+     * @param timestamp
+     * @return
+     */
+    public static LocalDate toDate(Long timestamp) {
+        return Instant.ofEpochMilli(timestamp).atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+
+    /**
+     * 计算某天所在月份的开始时间戳，也就是当月1日的0点0分0秒
+     *
+     * @param someDayInMonth
+     * @return 毫秒级别时间戳
+     */
+    public static Long firstSecondOfMonth(LocalDate someDayInMonth) {
+        LocalDate firstDayOfMonth = someDayInMonth.withDayOfMonth(1);
+        return DateUtil.toMilliseconds(firstDayOfMonth);
+    }
+
+    /**
+     * 计算某天所在月份的结束时间戳，也就是当月最后一日（28，30，21）的23点59分59秒
+     *
+     * @param someDayInMonth
+     * @return 毫秒级别时间戳
+     */
+    public static Long lastSecondOfMonth(LocalDate someDayInMonth) {
+        // 下个月的第一天
+        LocalDate firstDayOfNextMonth = someDayInMonth.plusMonths(1).withDayOfMonth(1);
+        // 下个月的第一天减去1秒，即为当月的最后一秒
+        return DateUtil.toMilliseconds(firstDayOfNextMonth) - (1 * 1000);
+    }
+
+    /**
+     * 计算某天所在年份的开始时间戳，也就是当年1月1日的0点0分0秒
+     *
+     * @param someDayInYear
+     * @return 毫秒级别时间戳
+     */
+    public static Long firstSecondOfYear(LocalDate someDayInYear) {
+        LocalDate firstDayOfYear = someDayInYear.withDayOfYear(1);
+        return DateUtil.toMilliseconds(firstDayOfYear);
+    }
+
+    /**
+     * 计算某天所在年份的结束时间戳，也就是当年12月31日的23点59分59秒
+     *
+     * @param someDayInYear
+     * @return 毫秒级别时间戳
+     */
+    public static Long lastSecondOfYear(LocalDate someDayInYear) {
+        LocalDate firstDayOfNextYear = someDayInYear.withDayOfYear(1).withYear(someDayInYear.getYear() + 1);
+        return DateUtil.toMilliseconds(firstDayOfNextYear) - (1 * 1000);
+    }
+
+    /**
+     * 将日期字符串转换为LocalDate
+     *
+     * @param dateText 时间字符串
+     * @param pattern  时间格式，比如：yyyy-MM-dd
+     * @return
+     */
+    public static LocalDate parseDate(String dateText, String pattern) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        return parseDate(dateText, formatter);
+    }
+
+    /**
+     * 将日期字符串转换为LocalDate
+     *
+     * @param dateText  时间字符串
+     * @param formatter
+     * @return
+     */
+    public static LocalDate parseDate(String dateText, DateTimeFormatter formatter) {
+        TemporalAccessor temporalAccessor = formatter.parse(dateText);
+        return LocalDate.from(temporalAccessor);
+    }
+
+    /**
+     * 将日期字符串转换为毫秒时间戳
+     *
+     * @param dateText 时间字符串
+     * @param pattern  时间格式，比如：yyyy-MM-dd
+     * @return
+     */
+    public static Long parseMilliseconds(String dateText, String pattern) {
+        LocalDate localDate = parseDate(dateText, pattern);
+        return DateUtil.toMilliseconds(localDate);
+    }
+
+    /**
+     * 将日期字符串转换为毫秒时间戳
+     *
+     * @param dateText  时间字符串
+     * @param formatter 时间格式，比如：yyyy-MM-dd
+     * @return
+     */
+    public static Long parseMilliseconds(String dateText, DateTimeFormatter formatter) {
+        LocalDate localDate = parseDate(dateText, formatter);
+        return DateUtil.toMilliseconds(localDate);
+    }
+
+
 }
--- mallen-common-utils/src/test/java/org/mallen/test/common/utils/DateUtilTest.java
+++ mallen-common-utils/src/test/java/org/mallen/test/common/utils/DateUtilTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 /**
  * @author mallen
@@ -15,5 +16,51 @@ public class DateUtilTest {
     public void testLocalDate2Milliseconds() {
         LocalDate date = LocalDate.of(2019, 12, 30);
         Assert.assertEquals(1577635200000L, (long)DateUtil.toMilliseconds(date));
+    }
+
+    @Test
+    public void testMilliseconds2ToDate() {
+        LocalDate translatedDate = DateUtil.toDate(1577635200000L);
+        LocalDate expectDate = LocalDate.of(2019, 12, 30);
+        Assert.assertEquals(expectDate, translatedDate);
+    }
+
+    @Test
+    public void testFirstSecondOfMonth() {
+        LocalDate date = LocalDate.of(2019, 12, 31);
+        Assert.assertEquals(1575129600000L, (long)DateUtil.firstSecondOfMonth(date));
+    }
+
+    @Test
+    public void testLastSecondOfMonth() {
+        LocalDate date = LocalDate.of(2019, 12, 31);
+        Assert.assertEquals(1577807999000L, (long)DateUtil.lastSecondOfMonth(date));
+    }
+
+    @Test
+    public void testFirstSecondOfYear() {
+        LocalDate date = LocalDate.of(2019, 12, 31);
+        Assert.assertEquals(1546272000000L, (long)DateUtil.firstSecondOfYear(date));
+    }
+
+    @Test
+    public void testLastSecondOfYear() {
+        LocalDate date = LocalDate.of(2019, 12, 31);
+        Assert.assertEquals(1577807999000L, (long)DateUtil.lastSecondOfYear(date));
+    }
+
+    @Test
+    public void testParseDate() {
+        LocalDate translatedDate = DateUtil.parseDate("2020-01-02", "yyyy-MM-dd");
+        LocalDate expectDate = LocalDate.of(2020, 1, 2);
+        Assert.assertEquals(expectDate, translatedDate);
+    }
+
+    @Test
+    public void testParseMilliseconds() {
+        Long translated = DateUtil.parseMilliseconds("2020-01-02", "yyyy-MM-dd");
+        // 2020.01.02的毫秒级别时间戳
+        Long expected = 1577894400000L;
+        Assert.assertEquals(expected, translated);
     }
 }
